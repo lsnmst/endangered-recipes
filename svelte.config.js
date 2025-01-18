30d26cbf0c69364b56e0926c4ff4c1c8476228ab
lsnmst <musetta.alessandro@gmail.com>
2025-01-18 19:05:33 +0100

    Fix config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-
 	kit: {
 		adapter: adapter({
-			pages: "build",
-			assets: "build",
-			precompress: false,
-			trailingSlash: 'always',
-			fallback: '404.html',
+			fallback: '404.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
