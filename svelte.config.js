b1ca0fabbb335441c69894b73941b3441d6828e8
lsnmst <musetta.alessandro@gmail.com>
2025-01-18 18:43:11 +0100

    process.env.BASE_PATH

--- svelte.config.js
+++ svelte.config.js
@@ -4,17 +4,17 @@ const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	
+
 	kit: {
 		adapter: adapter({
 			pages: "build",
-            assets: "build",
-            precompress: false,
-            trailingSlash: 'always',
+			assets: "build",
+			precompress: false,
+			trailingSlash: 'always',
 			fallback: '404.html',
 		}),
 		paths: {
-			base: dev ? '' : '/endangered-recipes',
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
